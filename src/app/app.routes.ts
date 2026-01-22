d1c5a52be44a6e22796ce5c466f8ad76befeeb4c
Zr0AM <103156517+Zr0AM@users.noreply.github.com>
2026-01-22 16:45:50 -0600

    Fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,8 @@
-import {RouterModule, Routes} from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from './home-component/home-component';
-import {NgModule} from '@angular/core';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-
-})
-
-export class AppRoutingModule {
-}

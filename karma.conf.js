ce1a16e9e54171eb420c980f7b3cc88c04464649
Zr0AM <103156517+Zr0AM@users.noreply.github.com>
2026-02-01 17:00:54 -0600

    fixxes

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function karma (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -24,7 +24,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/dnd-market-ui'),
+      dir: require('node:path').join(__dirname, './coverage/dnd-market-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
